7ab1eb86adf52a3ae86c3c8175231eb76616e7a0
yahyakyel <108628061+yahyakyel@users.noreply.github.com>
2023-03-06 23:04:12 +0300

    build2

---
+++ UI/Assets/js/UserScript.js
@@ -0,0 +1,51 @@
+﻿function AddUser() {
+    debugger
+    var FirstName = $('#firstname').val();
+    var LastName = $('#lastname').val();
+    var Email = $('#email').val();
+    var Phone = $('#phone').val();
+    var Message = $('#message').val();
+    if (FirstName == "") {
+        swal.fire("Hata!", "Adınızı Giriniz!", "error");
+    }
+    else if (LastName == "") {
+        swal.fire("Hata!", "Soyadınızı Giriniz!", "error");
+    }
+
+    else if (Email == "") {
+        swal.fire("Hata!", "Emailinizi Giriniz!", "error");
+    }
+
+    else if (Phone == "") {
+        swal.fire("Hata!", "Telefonunuzu Giriniz!", "error");
+    }
+
+    else {
+        debugger
+        var formData = new FormData();
+        //formData.append('avatar', newImage, FirstName + '_' + LastName + '.jpg');
+        formData.append('FirstName', FirstName);
+        formData.append('LastName', LastName);
+        formData.append('Email', Email);
+        formData.append('Phone', Phone);
+        formData.append('Message', Message);
+        $.ajax({
+            type: "POST",
+            url: '/Home/AddUser',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                swal.fire("Başarılı!", "Ön Kayıt İşleminiz Gerçekleştirildi.", "success");
+                $('#firstname').val("");
+                $('#lastname').val("");
+                $('#email').val("");
+                $('#phone').val("");
+                $('#message').val("");
+            },
+            error: function (request, status, error) {
+                swal.fire("Hata!", "Bir sorun ile karşılaşıldı!", "error");
+            }
+        });
+    }
+};
---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        EduContext db = new EduContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact() 
+        {
+            return View();
+        }
+        public ActionResult Services() 
+        {
+            return View();
+        } 
+        public ActionResult Gallery() 
+        {
+            return View();
+        }
+        public JsonResult AddUser(string FirstName, string LastName, string Email,  string Phone,string Message)
+        {
+            Users user=new Users()
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Email = Email,
+                Phone = Phone,
+                CreatedTime = DateTime.Now,
+                Message=Message,
+                userStatus= UserStatus.Waiting
+            };
+            db.Users.Add(user);
+            db.SaveChanges();
+     
+            string result = "Başarılı Şekilde Gönderildi...";
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
